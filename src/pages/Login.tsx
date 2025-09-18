df47c795e19bcb7f8500be556662b046d5a9b091
Darien-Cepani <60065437+Darien-Cepani@users.noreply.github.com>
2025-09-18 15:29:40 +0200

    [dyad] Refined login permissions - wrote 1 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,7 +9,7 @@ const Login = () => {
       provider: 'facebook',
       options: {
         redirectTo: `${window.location.origin}/`,
-        scopes: 'email,public_profile,instagram_basic',
+        scopes: 'email,public_profile',
       },
     });
     if (error) {
