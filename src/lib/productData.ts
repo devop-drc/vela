302065c2b31f5a49abdb315b22c9003c05f8a12a
Darien-Cepani <60065437+Darien-Cepani@users.noreply.github.com>
2025-09-23 14:39:11 +0200

    Reverted all changes back to version ba4d72163f175a87b8df7e02e4c014be5b1b696d

---
+++ src/lib/productData.ts
@@ -0,0 +1,7 @@
+export const clothingMaterials = [
+  "Cotton", "Polyester", "Linen", "Silk", "Wool", "Denim", "Spandex", "Nylon"
+];
+
+export const artMediums = [
+  "Oil on Canvas", "Watercolor", "Acrylic", "Print", "Photograph", "Sculpture", "Digital"
+];
